6dad0a32049905453060de51f60afcfc2991d788
harshdoiphode <harshdoiphode1308@gmail.com>
2023-09-25 18:54:04 +0530

    h

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Navbar from './component/navbar.js';
+import Services from './component/services.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
+    <Services/>
   </React.StrictMode>
 );
 
