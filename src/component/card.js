44c7624d2f5545ee60f60feb17b6ed82359eac3b
Legal-Guru <alqaedaak56@gmail.com>
2023-10-03 23:34:21 +0530

    6

--- src/component/card.js
+++ src/component/card.js
@@ -1,20 +1,19 @@
-import { Grid, Paper, Typography } from "@mui/material";
+import { Grid, Paper, Typography, Box } from "@mui/material";
 import "./home.css";
 
 const landing = () => {
   return (
     <>
-        <Grid item xs={3}>
-          <Paper elivation={6}>
-            <img
-              src={require("../images/Gabbar.jpg")}
-              className="home-img"
-            ></img>
+      <Grid item xs={3}>
+        <Paper elivation={6}>
+          <img src={require("../images/Gabbar.jpg")} className="home-img"></img>
+          <Box paddingX={2}>
             <Typography variant="h6" component="h2">
               Gabbar Singh
             </Typography>
-          </Paper>
-        </Grid>
+          </Box>
+        </Paper>
+      </Grid>
     </>
   );
 };
