9dfe5f562b1691b6fe3161064718c0d3844ecadf
harshdoiphode <harshdoiphode1308@gmail.com>
2023-10-03 22:13:13 +0530

    f

--- src/component/landing.js
+++ src/component/landing.js
@@ -1,9 +1,37 @@
-import "./home.css";
+import React from 'react';
+import"./home.css"
 
 const landing = () =>{
     return(
         <>
-            <h1>Landing Page</h1>
+             
+      <div class="landing-page">
+        <header>
+          <div class="container">
+            <a href="#" class="logo">Your <b>Website</b></a>
+            <ul class="links">
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Work</li>
+              <li>Info</li>
+              <li>Get Started</li>
+            </ul>
+          </div>
+        </header>
+        <div class="content">
+          <div class="container">
+            <div class="info">
+              <h1>Looking For Inspiration</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit nihil ullam nesciunt quidem iste, Repellendus odit nihil</p>
+              <button>Button name</button>
+            </div>
+            <div class="image">
+              <img src="https://i.postimg.cc/65QxYYzh/001234.png"/>
+            </div>
+          </div>
+        </div>
+      </div>
+      
         </>
     )
 }
