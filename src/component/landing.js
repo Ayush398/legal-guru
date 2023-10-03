ea75ecd411047ef2548be07b1a171fe481a47210
Legal-Guru <alqaedaak56@gmail.com>
2023-10-03 22:18:43 +0530

    6

--- src/component/landing.js
+++ src/component/landing.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import"./home.css"
+import React from "react";
+import "./home.css";
 
-const landing = () =>{
-    return(
-        <>
-             
-      <div class="landing-page">
+const landing = () => {
+  return (
+    <>
+      <div className="landing-page">
         <header>
-          <div class="container">
-            <a href="#" class="logo">Your <b>Website</b></a>
-            <ul class="links">
+          <div className="container">
+            <a href="#" className="logo">
+              Your <b>Website</b>
+            </a>
+            <ul className="links">
               <li>Home</li>
               <li>About Us</li>
               <li>Work</li>
@@ -18,22 +19,25 @@ const landing = () =>{
             </ul>
           </div>
         </header>
-        <div class="content">
-          <div class="container">
-            <div class="info">
+        <div className="content">
+          <div className="container">
+            <div className="info">
               <h1>Looking For Inspiration</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit nihil ullam nesciunt quidem iste, Repellendus odit nihil</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repellendus odit nihil ullam nesciunt quidem iste, Repellendus
+                odit nihil
+              </p>
               <button>Button name</button>
             </div>
-            <div class="image">
-              <img src="https://i.postimg.cc/65QxYYzh/001234.png"/>
+            <div className="image">
+              <img src="https://i.postimg.cc/65QxYYzh/001234.png" />
             </div>
           </div>
         </div>
       </div>
-      
-        </>
-    )
-}
+    </>
+  );
+};
 
 export default landing;
