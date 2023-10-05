9775e27d6d8018dd87079c162c0cde2f441d349b
Legal-Guru <alqaedaak56@gmail.com>
2023-10-05 18:54:12 +0530

    7

--- src/component/card.js
+++ src/component/card.js
@@ -1,21 +1,12 @@
 import { Grid, Paper, Typography, Box } from "@mui/material";
 import "./home.css";
 
-const landing = () => {
+const card = () => {
   return (
     <>
-      <Grid item xs={3}>
-        <Paper elivation={6}>
-          <img src={require("../images/Gabbar.jpg")} className="home-img"></img>
-          <Box paddingX={2}>
-            <Typography variant="h6" component="h2">
-              Gabbar Singh
-            </Typography>
-          </Box>
-        </Paper>
-      </Grid>
+      
     </>
   );
 };
 
-export default landing;
+export default card;
--- src/component/home.js
+++ src/component/home.js
@@ -2,6 +2,8 @@ import "./home.css";
 import Card from "./card.js";
 import Landing from "./landing.js";
 import Testimonial from "./testimonial";
+import FAQ from "./faq"
+import Footer from "./footer.js"
 import { Grid } from "@mui/material";
 import { Container } from "@mui/material";
 
@@ -9,15 +11,10 @@ const home = () => {
   return (
     <>
     <Landing />
-      <Container>
-        <Grid container spacing={5}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </Grid>
-      </Container>
+    <Card/>
     <Testimonial/>
+    <FAQ/>
+    <Footer/>
     </>
   );
 };
